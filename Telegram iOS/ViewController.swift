88cba82aac2fb3f3901119f34de329048414646a
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-10-10 20:14:37 +0200

    Initial Commit

---
+++ Telegram iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Telegram iOS
+//
+//  Created by eldorbek nusratov on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
