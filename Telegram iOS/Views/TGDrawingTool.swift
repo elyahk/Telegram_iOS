5d1441aea6b052e9802cc7edb4bc932e03b150d7
Eldorbek Nusratov <eldorbek.nusratov.98@gmail.com>
2022-10-14 21:26:31 +0200

    Remove pencil Kit

--- Telegram iOS/Views/TGDrawingTool.swift
+++ Telegram iOS/Views/TGDrawingTool.swift
@@ -8,25 +8,49 @@
 import UIKit
 
 class TGDrawingTool: UIView {
-    private(set) lazy var baseToolImageView: UIImageView = {
-        let view = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: 17.0, height: 93).universal())
-        view.backgroundColor = .clear
-        view.contentMode = .scaleToFill
-        view.image = Images.toolBasePen.image
+    public var type: ToolType {
+        didSet { updateType() }
+    }
+
+    lazy var pencilImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFill
+        image.image = type.baseImage
+
+        return image
+    }()
+
+    lazy var tipImageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.image = type.tipImage
+        view.contentMode = .scaleAspectFill
 
         return view
     }()
 
-    private(set) lazy var tipToolImageView: UIImageView = {
-        let view = UIImageView(frame: CGRect(x: 0.0, y: 0.0, width: 17.0, height: 12.5).universal())
-        view.backgroundColor = .clear
-        view.contentMode = .scaleToFill
-        view.image = Images.undo.image
+    lazy var whiteRectangleleView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 1.0
+        view.clipsToBounds = true
 
         return view
     }()
+
+    convenience init(type: ToolType) {
+        self.init(frame: .zero)
+
+        self.type = type
+        setupSubviews()
+        updateType()
+    }
 
     override init(frame: CGRect) {
+        type = .pen
+
         super.init(frame: frame)
 
         setupSubviews()
@@ -36,11 +60,34 @@ class TGDrawingTool: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
-    private func setupSubviews() {
-        self.addSubview(baseToolImageView)
-        self.addSubview(tipToolImageView)
+    private func updateType() {
+        pencilImageView.image = type.baseImage
+        tipImageView.image = type.tipImage
+    }
 
-        self.layoutIfNeeded()
+    private func setupSubviews(){
+        addSubview(pencilImageView)
+        addSubview(tipImageView)
+        addSubview(whiteRectangleleView)
+
+
+        NSLayoutConstraint.activate([
+            pencilImageView.topAnchor.constraint(equalTo: topAnchor),
+            pencilImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            pencilImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            pencilImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            tipImageView.topAnchor.constraint(equalTo: topAnchor),
+            tipImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            tipImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            tipImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+
+            whiteRectangleleView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: universalWidth(1.5)),
+            whiteRectangleleView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -universalWidth(1.5)),
+            whiteRectangleleView.topAnchor.constraint(equalTo: topAnchor,constant: universalHeight(35.0)),
+            whiteRectangleleView.heightAnchor.constraint(equalToConstant: universalHeight(3.0))
+
+        ])
     }
 }
 
